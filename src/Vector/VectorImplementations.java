f410e7b261af07392a8e60357d2b9f917e44e4e7
Devendra <devendra103@gmail.com>
2022-03-04 00:59:22 -0500

    vector

---
+++ src/Vector/VectorImplementations.java
@@ -0,0 +1,13 @@
+package Vector;
+
+import java.util.Vector;
+
+public class VectorImplementations {
+    public static void main(String[] args){
+        Vector<Integer> v = new Vector<>();
+        v.add(3);
+        v.add(55);
+
+
+    }
+}
